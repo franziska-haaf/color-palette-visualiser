745aa649dacde2b6410d7b2ca209c3e8c72defe4
Franziska Haaf <franziska.haaf@rewe-group.com>
2026-03-12 11:25:30 +0100

    UI telling the code to create a palette in figma now works

--- code.ts
+++ code.ts
@@ -12,63 +12,125 @@ class ColorVariable {
     }
 }
 
-class ColorGroup {
+class MyColorPalette {
   name: string;
-  colors: ColorVariable[];
+  colors: ColorVariable[] = [];
+  
   constructor(theName: string, theColors: ColorVariable[]) {
     this.name = theName;
     this.colors = theColors;
   } 
 }
 
-/**
- * Initialize the plugin by fetching local variable collections and sending them to the UI.
- */
+
 (async () => {
+  // Load the local variable collections
   const localCollections = await figma.variables.getLocalVariableCollectionsAsync();
+
+  // Send the available list to the UI todo use this
   let localCollectionsFormatted = [{}]
   localCollections.forEach(collection => {
     localCollectionsFormatted.push({ name: collection.name, id: collection.id })
   })
-  figma.ui.postMessage({ localCollections: localCollectionsFormatted });
-})();
+  // Keep both keys for compatibility with existing UI code.
+  figma.ui.postMessage({
+    localCollections: localCollectionsFormatted,
+    localCollectionsFormatted,
+  });
 
-
-async function getColorPalettes() {
-  const localCollections = await figma.variables.getLocalVariableCollectionsAsync();
-
+  // todo for testing purposes only first collection is loaded
   const firstCollection = localCollections[0];
-  if (!firstCollection) {
-    return;
-  }
 
-  const colorGroups: ColorGroup[] = [];
+  const colorPalettesOfCollection: MyColorPalette[] = [];
 
-  for (const variableId of firstCollection.variableIds) {
+  for (let variableId of firstCollection.variableIds) {
     const variable = await figma.variables.getVariableByIdAsync(variableId);
 
     // If it's a color, check for an existing group or create one and add it either way.
     if (variable?.resolvedType === 'COLOR') {
       const variableNameHierarchy = variable.name.split('/');
-      const groupName = variableNameHierarchy.length > 1
+      const paletteName = variableNameHierarchy.length > 1
         ? variableNameHierarchy[variableNameHierarchy.length - 2]
-        : 'Ungrouped'; // If no group exists
+        : 'Ungrouped'; // If no variable grouping exists
 
-      // Check if a group with this name already exists.
-      let existingColorGroup = colorGroups.find(group => group.name === groupName);
+      // Check if a palette with this name already exists. 
+      let existingColorPalette = colorPalettesOfCollection.find(palette => palette.name === paletteName);
 
-      if (!existingColorGroup) { 
-        existingColorGroup = new ColorGroup(groupName, []);
-        colorGroups.push(existingColorGroup);
+      if (!existingColorPalette) { 
+        existingColorPalette = new MyColorPalette(paletteName, []);
+        colorPalettesOfCollection.push(existingColorPalette);
       }
 
-      existingColorGroup.colors.push(new ColorVariable(variable.name, variable.id));
+      existingColorPalette.colors.push(new ColorVariable(variable.name, variable.id));
     }
   }
 
-  console.log('colorGroups', colorGroups);
-  figma.ui.postMessage({ colorGroups }); 
+  console.log('colorPalettesOfCollection', colorPalettesOfCollection);
+  // Keep both keys for compatibility with existing UI code.
+  figma.ui.postMessage({
+    
+    colorPalettesOfCollection: colorPalettesOfCollection,
+  }); 
 
-}
-getColorPalettes();
+  // Create a palette for the given palette
+  figma.ui.onmessage = (msg) => {
+    // Support both old and new UI payload formats.
+    if (msg.type === 'create-palette') {
+      const paletteName = msg.palette ?? msg.paletteName;
+      console.log('Creating palette for', paletteName);
+      //createPalette(paletteName);
+    }
+  }
 
+})();
+
+
+/* 
+
+function createPalette(colorPalette) {
+    debugger;
+    // Create a frame named {paletteName}
+    let paletteFrame = figma.createFrame()
+    paletteFrame.name = colorPalette.name
+
+    // Create a text with the {paletteName}
+    let paletteNameText = figma.createText()
+    paletteNameText.characters = colorPalette.name
+
+    // Add a frame named "Colors"
+    let colorsFrame = figma.createFrame()
+    colorsFrame.name = "Colors"
+
+    // Add a frame "Color Wrapper" for each color
+    colorPalette.colors.forEach(async (color) => { // todo check was hier tatsächlich ankommt
+      debugger;
+      let colorWrapper = figma.createFrame()
+      colorWrapper.name = "Color Wrapper"
+
+      // Add the name of the color. E.g. "100", "200" etc.
+      let colorValueText = figma.createText()
+      colorValueText.characters = color.name
+      colorWrapper.appendChild(colorValueText)
+
+      // Create a frame to be filled with the color
+      let colorFrame = figma.createFrame()
+      colorFrame.name = "Color"
+      // Load the variable for the color id
+      const fillColorVariable = await figma.variables.getVariableByIdAsync(color.id);
+      // Set it as a fill. This complicated construct is from the Figma doc
+      const fillsCopy = clone(colorFrame.fills)
+      fillsCopy[0] = figma.variables.setBoundVariableForPaint(fillsCopy[0], 'color', fillColorVariable)
+      colorFrame.fills = fillsCopy
+      colorWrapper.appendChild(colorFrame)
+
+      colorsFrame.appendChild(colorWrapper)
+    })
+
+
+    // Fills and strokes must be set via their immutable arrays
+    // node.setBoundVariable('width', widthVariable)
+    // const fillsCopy = clone(node.fills)
+    // fillsCopy[0] = figma.variables.setBoundVariableForPaint(fillsCopy[0], 'color', colorVariable)
+    // node.fills = fillsCopy
+
+  } */
